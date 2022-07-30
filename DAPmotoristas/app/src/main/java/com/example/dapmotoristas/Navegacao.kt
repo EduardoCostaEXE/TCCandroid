81a77c3d5553788edd4ffcd2c958bb44e38c5f10
Rodolfo Brant Salomé <108497011+RodolfoSalome@users.noreply.github.com>
2022-07-29 23:28:37 -0300

    Checkpoint!
    Checkpoint!

---
+++ DAPmotoristas/app/src/main/java/com/example/dapmotoristas/Navegacao.kt
@@ -0,0 +1,11 @@
+package com.example.aplicativos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Navegacao : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_navegacao)
+    }
+}
