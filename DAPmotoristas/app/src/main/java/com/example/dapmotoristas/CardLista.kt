6463fff5468d431dd511766a3e3a8811518e70d2
Sabrina Costa <sabrina.costa06@outlook.com>
2022-08-06 00:46:50 -0300

    Padronizando ícones e restaurando telas apagadas

---
+++ DAPmotoristas/app/src/main/java/com/example/dapmotoristas/CardLista.kt
@@ -0,0 +1,11 @@
+package com.example.dapmotoristas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardLista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_lista)
+    }
+}
---
+++ DAPmotoristas/app/src/main/java/com/example/dapmotoristas/ListaDeAlunos.kt
@@ -0,0 +1,11 @@
+package com.example.dapmotoristas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaDeAlunos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_de_alunos)
+    }
+}
