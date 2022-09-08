93ac6b24acd045de1fb7b7c8dc2396ccaaf59295
Eduardo Cabral Costa <eduardocabral95@gmail.com>
2022-09-07 22:52:48 -0300

    Primeiros ajustes para TomTom
    Substituição inicial do GoogleMaps para TomTom, com erro final
    
    Co-Authored-By: Rodolfo Brant Salomé <108497011+RodolfoSalome@users.noreply.github.com>
    Co-Authored-By: Sabrina Costa <79335261+SaCosta-pckt@users.noreply.github.com>

--- DAPmotoristas/app/src/main/java/com/example/dapmotoristas/Navegacao.kt
+++ DAPmotoristas/app/src/main/java/com/example/dapmotoristas/Navegacao.kt
@@ -1,24 +1,18 @@
 package com.example.dapmotoristas
 
 import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
+//Tomtom
+import com.tomtom.sdk.maps.display.MapOptions
+import com.tomtom.sdk.maps.display.ui.MapFragment
+import com.tomtom.sdk.maps.display.TomTomMap
 
-class Navegacao : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
-    private lateinit var map: GoogleMap
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var lastLocation: Location
+class Navegacao : AppCompatActivity() {
+
+    private lateinit var tomTomMap: TomTomMap
+    private val APIKEY = "2XhCWUOz93KHvOjIGSoZ6D8liAgYjcrq"
 
     //Requisita permissão do usuário para acessar a localização dele
     companion object {
@@ -29,65 +23,34 @@ class Navegacao : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerCli
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_navegacao)
 
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.mapa) as? SupportMapFragment
-        mapFragment?.getMapAsync(this)
+        //Add map fragment
+        val mapOptions = MapOptions(mapKey = APIKEY)
+        val mapFragment = MapFragment.newInstance(mapOptions)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.map_container, mapFragment)
+            .commit()
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mapFragment.getMapAsync { map ->
+            tomTomMap = map
+            enableUserLocation()
+            setUpMapListeners()
+        }
     }
 
-    //COLOQUEM AS LOCALIZAÇÕES AQUI
-        //1 - CRIEM A VARIÁVEL COM A LATITUDE E A LONGITUDE DA LOCALIZAÇÃO
-        //2 - ADICIONEM O MÉTODO DO "googleMap.addMarker" PARA MARCAR NO MAPA
-            //.position = nome da variável
-            //.title = você que decide HUEHUEHUEHUEUE
-    override fun onMapReady(map: GoogleMap) {
+    private fun setUpMapListeners() {
 
-        //Faz aparecer os botões de zoom e permite ao usuário interagir melhor
-        map.getUiSettings().setZoomControlsEnabled(true)
-
-        val Unip = LatLng(-23.25520814200808, -45.94855886683163)
-        val Casa = LatLng(28.3948982,-81.6029011)
-        map.addMarker(
-            MarkerOptions()
-                .position(Unip)
-                .title("UNIP")
-        )
-        map.addMarker(
-            MarkerOptions()
-                .position(Casa)
-                .title("FREEZA! POR QUE VOCÊ MATOU O KURIRIN?")
-        )
-
-        //Abrir o mapa centralizado no marcador e com zoom nele
-        map.animateCamera(CameraUpdateFactory.newLatLngZoom(Unip, 15.0f))
-
-        setUpMap()
     }
+
+    private fun enableUserLocation() {
+
+    }
+
 
     //Verifica se o usuário permitiu o compartilhamento de localização, caso não tenha, é solicitado novamente
-    private fun setUpMap() {
+    private fun permission() {
         if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMITION_REQUEST_CODE)
             return
         }
-
-        map.isMyLocationEnabled = true
-
-        fusedLocationClient.lastLocation.addOnSuccessListener(this) {  location ->
-            if (location != null){
-                lastLocation = location
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                placeMarkerOnMap(currentLatLng)
-//                Colocar o foco no usuário
-//                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 14.0f))
-            }
-        }
     }
-
-    private fun placeMarkerOnMap(location: LatLng) {
-        val markerOptions = MarkerOptions().position(location)
-        map.addMarker(markerOptions)
-    }
-
-    override fun onMarkerClick(p0: Marker) = false
 }
